340a99834b9be4e7b527dd3cbbb73ec8a4c338d7
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-06-04 04:46:38 +0000

    feat: initialize NestJS application and configure pre-commit hooks
    - Initialize NestJS application structure.
    - Implement Husky and lint-staged to automate formatting, linting, and testing on pre-commit.

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import util from 'node:util';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  console.log(util.inspect(app, { depth: null, colors: true }));
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
