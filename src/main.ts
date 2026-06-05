e770285fc73864a86f0464bb57d3f133b87f666c
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-06-05 04:45:13 +0000

    added initial files for users module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import util from 'node:util';
+// import util from 'node:util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(util.inspect(app, { depth: null, colors: true }));
+  // console.log(util.inspect(app, { depth: null, colors: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
