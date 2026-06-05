058bca5da69c631fa7844d3765663915bf4b0c60
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-06-05 10:23:54 +0000

    controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /**
+   * GET /users
+   * GET /users/:id
+   * POST /users
+   * PATCH /users/:id
+   * DELETE /users/:id
+   */
+
+  // GET /users
+  @Get()
+  findAll(@Query('role') role?: 'ADMIN' | 'DEV' | 'INTERN') {
+    return 
+  }
+
+  // GET /users/devs
+  @Get('devs')
+  findDevs() {
+    return [];
+  }
+
+  // GET /users/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /users
+  @Post()
+  create(@Body() createUser: { id: string, name: string, role: string }) {
+    return createUser;
+  }
+
+  // POST /users/alt
+  @Post('alt')
+  createAlt(
+    @Body('id') id: string,
+    @Body('name') name: string,
+  ) {
+    return {
+      id,
+      name
+    };
+  }
+
+  // PATCH /users/:id
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUser: {}) {
+    return { id, ...updateUser };
+  }
+
+  // DELETE /users/:id
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
